ded1bb091186b4972eac0d0e2e61200e3e31aa4a
Valtkf <valentin.vgregorio@gmail.com>
2024-09-10 17:30:51 +0200

    F01 : Add BgApp in images file

--- src/assets/components/Card.jsx
+++ src/assets/components/Card.jsx
@@ -3,10 +3,17 @@ import StartChallengeButton from "./StartChallengeButton";
 import BgRed1 from "./images/BgRed1.png";
 import BgRed2 from "./images/BgRed2.png";
 import BgBlack from "./images/BgBlack.png";
+import BgApp from "./images/BgApp.png";
+
 export default function Card() {
   return (
     <div className="flex justify-center items-center flex-wrap gap-y-8 gap-x-8">
-      <div className="flex flex-col items-center border-[1px] border-slate-800 rounded-3xl w-[411px] h-[475px] text-2xl text-slate-800 font-sans p-4">
+      <img
+        src={BgApp}
+        alt=""
+        className="fixed top-0 left-0 right-0 bottom-0 min-h-screen w-full object-cover z-0"
+      />
+      <div className="z-10 flex flex-col items-center border-[1px] border-[#E4E4EB] bg-white rounded-3xl w-[411px] h-[475px] text-2xl text-slate-800 font-sans p-4">
         <div className="  rounded-2xl w-[375px] h-[263px]">
           <img
             src={BgRed1}
@@ -30,7 +37,7 @@ export default function Card() {
           <DropDownMenu />
         </div>
       </div>
-      <div className="flex flex-col items-center border-[1px] border-slate-800 rounded-3xl w-[411px] h-[475px] text-2xl text-slate-800 font-sans p-4">
+      <div className="z-10 flex flex-col items-center border-[1px] border-[#E4E4EB] bg-white rounded-3xl w-[411px] h-[475px] text-2xl text-slate-800 font-sans p-4">
         <div className="  rounded-2xl w-[375px] h-[263px]">
           <img
             src={BgBlack}
@@ -39,7 +46,7 @@ export default function Card() {
           />
         </div>
         <div className="flex justify-between items-end w-full">
-          <h1 className="text-xl mt-4 mb-2">Raycast Wallpaper #1</h1>
+          <h1 className="text-xl mt-4 mb-2">Raycast Wallpaper #2</h1>
           <h2 className="flex justify-center items-center text-xs font-medium bg-slate-200 w-[62px] h-[26px] rounded-md">
             premium
           </h2>
@@ -54,7 +61,7 @@ export default function Card() {
           <DropDownMenu />
         </div>
       </div>
-      <div className="flex flex-col items-center border-[1px] border-slate-800 rounded-3xl w-[411px] h-[475px] text-2xl text-slate-800 font-sans p-4">
+      <div className="z-10 flex flex-col items-center border-[1px] border-[#E4E4EB] bg-white rounded-3xl w-[411px] h-[475px] text-2xl text-slate-800 font-sans p-4">
         <div className="  rounded-2xl w-[375px] h-[263px]">
           <img
             src={BgRed2}
@@ -63,7 +70,7 @@ export default function Card() {
           />
         </div>
         <div className="flex justify-between items-end w-full">
-          <h1 className="text-xl mt-4 mb-2">Raycast Wallpaper #1</h1>
+          <h1 className="text-xl mt-4 mb-2">Raycast Wallpaper #3</h1>
           <h2 className="flex justify-center items-center text-xs font-medium bg-slate-200 w-[62px] h-[26px] rounded-md">
             premium
           </h2>
